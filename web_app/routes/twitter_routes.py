cb593184ec66a79212e244d1caf936d187797b63
Afroman <makoanoble@gmail.com>
2020-06-08 22:14:20 -0600

    Added webpage for after tweets are loaded

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -31,12 +31,12 @@ def get_user(screen_name=None):
 
     # basilica_api = basilica_api_client()
 
-    # all_tweet_texts = [status.full_text for status in statuses]
-    # embeddings = list(basilica_api_client.embed_sentences(all_tweet_texts, model="twitter"))
-    # print("NUMBER OF EMBEDDINGS", len(embeddings))
+    all_tweet_texts = [status.full_text for status in statuses]
+    embeddings = list(basilica_api_client.embed_sentences(all_tweet_texts, model="twitter"))
+    print("NUMBER OF EMBEDDINGS", len(embeddings))
 
     # TODO: explore using the zip() function maybe...
-    # counter = 0
+    counter = 0
     for status in statuses:
         print(status.full_text)
         print("----")
@@ -44,13 +44,12 @@ def get_user(screen_name=None):
         db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
         db_tweet.user_id = status.author.id # or db_user.id
         db_tweet.full_text = status.full_text
-        embedding = basilica_api_client.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
-        # embedding = embeddings[counter]
+        embedding = embeddings[counter]
         print(len(embedding))
         db_tweet.embedding = embedding
         db.session.add(db_tweet)
-        # counter+=1
+        counter+=1
     db.session.commit()
     #breakpoint()
-    return "OK"
-    #return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
+    # return "OK"
+    return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
